c2f10ee108caad8ac7c06abc7ec8c792c74fdddf
andreyskorchenko <andreyskorchenko@gmail.com>
2022-09-05 01:06:23 +0300

    improve(pages): added markup and styles

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,3 +1,19 @@
-export const Products = () => {
-  return <h1>Products page</h1>;
-};
+import { useEffect } from 'react';
+import { observer } from 'mobx-react-lite';
+import { Product } from '../../components';
+import ProductsState from '../../store/ProductsState';
+import styles from './Products.module.scss';
+
+export const Products = observer(() => {
+  useEffect(() => {
+    ProductsState.fetchData();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      {ProductsState.list.map((product) => (
+        <Product info={product} key={product.id} />
+      ))}
+    </div>
+  );
+});
