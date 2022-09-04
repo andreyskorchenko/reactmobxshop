d2eefc4b362b9812db954c53b210b4beb60d2ab5
andreyskorchenko <andreyskorchenko@gmail.com>
2022-09-04 23:30:45 +0300

    feature(app): added entry point in application and routes

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { MainLayout } from './layouts';
+import { Products, Basket } from './pages';
+
+const App: FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Products />} />
+        <Route path={'basket'} element={<Basket />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
