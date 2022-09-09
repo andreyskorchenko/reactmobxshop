48a6531a58311b3ef42553c184c40c99b54e7f4c
andreyskorchenko <andreyskorchenko@gmail.com>
2022-09-09 23:22:33 +0300

    feature(pages): implemented basket page

--- src/pages/Basket/Basket.tsx
+++ src/pages/Basket/Basket.tsx
@@ -1,3 +1,24 @@
-export const Basket = () => {
-  return <h1>Basket page</h1>;
-};
+import { FC } from 'react';
+import { observer } from 'mobx-react-lite';
+import BasketStore from '@/store/BasketStore';
+import { BasketProduct } from '@/components';
+import { convertPrice } from '@/helpers';
+import styles from './Basket.module.scss';
+
+export const Basket: FC = observer((): JSX.Element => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.items}>
+        {BasketStore.list.map((product) => (
+          <BasketProduct info={product} key={product.id} />
+        ))}
+      </div>
+
+      <div className={styles.total}>
+        <h3>
+          Total price: <span>{convertPrice(BasketStore.totalPrice)}</span>
+        </h3>
+      </div>
+    </div>
+  );
+});
