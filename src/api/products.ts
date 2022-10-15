84fb4e2574d7a2575b57b4862dc82556a291cff6
andreyskorchenko <andreyskorchenko@gmail.com>
2022-10-15 20:14:53 +0300

    added optional param abortSignal for request cancelation

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,8 +1,12 @@
 import axios from 'axios';
 import { IProduct } from '@/types';
 
-export const getProducts = async (): Promise<IProduct[]> => {
+export const getProducts = async (
+  abortSignal?: AbortSignal
+): Promise<IProduct[]> => {
   return await (
-    await axios.get<IProduct[]>('http://localhost:3000/products')
+    await axios.get<IProduct[]>('http://localhost:3000/products', {
+      signal: abortSignal,
+    })
   ).data;
 };
