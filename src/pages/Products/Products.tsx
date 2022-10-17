6275ac083dfd1f692c1cfe3e30173128a18e3675
andreyskorchenko <andreyskorchenko@gmail.com>
2022-10-17 19:36:33 +0300

    refactoring and using the abortion signal to cancel the request when leaving the page

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -2,16 +2,19 @@ import { useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import { Product, Spinner } from '@/components';
 import ProductsStore from '@/store/ProductsStore';
+import { useAbortSignal } from '@/hooks';
 import styles from './Products.module.scss';
 
 export const Products = observer(() => {
+  const abortSignal = useAbortSignal();
+
   useEffect(() => {
-    ProductsStore.fetchData();
+    ProductsStore.fetchData(abortSignal);
   }, []);
 
   return (
     <div className={styles.container}>
-      {ProductsStore.isLoading ? (
+      {ProductsStore.pending ? (
         <Spinner />
       ) : (
         ProductsStore.list.map((product) => (
